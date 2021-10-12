f230d0ea5a95f464bc3ee6c280bbd4ed9a2e480b
Oluwaseun Alo <oluwaseunalo4@gmail.com>
2021-10-12 10:32:09 +0200

    modify the task.js

--- client/task2.js
+++ client/task2.js
@@ -84,3 +84,25 @@ const getData = async () => {
 } 
 
 
+// Expressing the postData callback function
+const postData = async (url = '', data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials:'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+            body: JSON.stringify(data),
+        });
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }
+    catch(error){
+        console.log('error', error);
+    }
+}
+
+
